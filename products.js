bf926b3ee6ab430f876ff750a186b886703bfd9b
nidhi0207 <68142055+nidhi0207@users.noreply.github.com>
2021-11-19 11:18:40 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,83 @@
+
+var products = {
+  1201: {
+    name : "pizza",
+    desc : "Thin Crust And Double Burst Cheese Is The Perfect Treat In The Evening.",
+    img :  "menu-1.jpg",
+    price : 350
+  },
+  1202: {
+    name : "fruit pudding",
+    desc : "Pudding Full Of Fruits And Flavours The Perfect Dessert With A Real Feeling.",
+    img : "menu-8.jpg",
+    price : 250
+  },
+  1203: {
+    name : "Mojito",
+    desc : "Epitome Of Cocktail, Stripped Down To Just Bare Essentials Of Rum, Lime Juice, Sugar, Soda Water.",
+    img : "menu-9.jpg",
+    price : 150
+  },
+  1204: {
+    name : "cup cakes",
+    desc : "Small In Size But Big In Richness The Real Taste Of Fruits. Treat Yourself.",
+    img : "menu-6.jpg",
+    price : 200
+  },
+  1205: {
+    name : "Ice cream",
+    desc : "A Rich, Sweet, Creamy Frozen Food Made From Variously Flavored Cream And Milk Products Churned.",
+    img : "menu-4.jpg",
+    price : 100
+  },
+  1206: {
+    name : "Fruit Drinks",
+    desc : "Real And Rich Taste Of Fruits With A Special Touch Of Spices In The Drinks.",
+    img : "menu-7.jpg",
+    price : 200
+  },
+  1207: {
+    name : "Delicious Cake",
+    desc : "Melt-In-The-Mouth Texture Of Cake Comes From Gas Bubbles. Rich And Real In Taste.",
+    img : "menu-5.jpg",
+    price : 450
+  },
+  1208: {
+    name : "Burger",
+    desc : "Fully Loaded Cheese Burger With Different Spices And Herbs. Feel The Temptations.",
+    img : "burger.jpg",
+    price : 65
+  },
+  1209: {
+    name : "brownie",
+    desc : "Prepared And Presented In A Variety Of Ways Espresso, French Press, Caffè Latte",
+    img : "brownie.jpg",
+    price : 150
+  },
+  1210: {
+    name : "coffee",
+    desc : "Prepared And Presented In A Variety Of Ways Espresso, French Press, Caffè Latte",
+    img : "Coffee.jpeg",
+    price : 150
+  },
+
+  1211: {
+    name : "Pasta",
+    desc : "Several Layers Of Lasagna Sheets With Sauce And Other Ingredients. Enjoy It",
+    img : "menu_pasta.jpg",
+    price : 200
+  },
+  1212: {
+    name : "Pasta",
+    desc : "Sprinkle Of Different Herbs And Spices And Fully Loaded Cream.Treat Yourself.",
+    img : "french_fries.jpg",
+    price : 200
+  },
+  
+
+  
+  
+
+  
+
+};
